9d9e5f3ac38f08f89fca517f286ab93ebdb05150
Artem Zadorov <artemzadorov@Artems-Laptop.local>
2024-12-03 10:36:40 +0300

    fix

--- internal/storage/target/target.go
+++ internal/storage/target/target.go
@@ -33,23 +33,27 @@ func New(ctx context.Context, dbConn string) *TargetDB {
 
 }
 
+// Execute selected report tranform report data to models.Data
 func (t *TargetDB) ExecuteQuery(ctx context.Context, query string) (models.Data, error) {
 
+	// DB request
 	rows, err := t.pool.Query(ctx, query)
 	if err != nil {
 		log.Print(err)
 		return models.Data{}, err
 	}
 
-	fieldDescriptions := rows.FieldDescriptions()
-
-	columns := make([]string, len(fieldDescriptions))
+	// future models.Data columns
+	var columns = []string{}
 	var valueRows = [][]any{}
 
+	// DB table columns to models.Data columns
+	fieldDescriptions := rows.FieldDescriptions()
 	for _, v := range fieldDescriptions {
 		columns = append(columns, v.Name)
 	}
 
+	// DB table values to models.Data columns list
 	for rows.Next() {
 		values, err := rows.Values()
 		if err != nil {
@@ -58,6 +62,7 @@ func (t *TargetDB) ExecuteQuery(ctx context.Context, query string) (models.Data,
 		valueRows = append(valueRows, values)
 	}
 
+	// create answer
 	results := models.Data{
 		Columns: columns,
 		Values:  valueRows,
